5bbf0f600257cb15e6ad69547fff879ecc04157f
n00bpr0gram3r <abdulb4s1tbakz@gmail.com>
2025-02-26 17:39:18 +0300

    remove unused images; add new JavaScript and CSS dependencies; update index.html for new assets

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+// filepath: /d:/AKC/Website/Auto-King-Company/src/assets/js/main.js
+$(document).ready(function () {
+  // Initialize WOW.js
+  new WOW().init();
+
+  // Initialize other plugins
+  $('.counter').counterUp();
+  $('.owl-carousel').owlCarousel();
+  $('#datetimepicker').datetimepicker();
+});
