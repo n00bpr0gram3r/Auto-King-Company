1cac23f9bb2ca3c72852862d0547e0253750c579
n00bpr0gram3r <abdulb4s1tbakz@gmail.com>
2025-03-09 00:54:01 +0300

    enhance carousel component with improved styling and dynamic content; add toggle functionality for service details

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
+  expandedSections: { [key: string]: boolean } = {
+    'quality': false,
+    'expert': false,
+    'equipment': false
+  };
 
+  toggleSection(section: string) {
+    this.expandedSections[section] = !this.expandedSections[section];
+  }
+
+  isExpanded(section: string): boolean {
+    return this.expandedSections[section];
+  }
 }
